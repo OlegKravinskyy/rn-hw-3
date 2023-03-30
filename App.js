7d4bdc0f814c312b0cb2c1c88e08a7a92b569c85
OlegKravinskyy <kravinskyy@gmail.com>
2023-03-30 23:07:56 +0300

    added files

---
+++ App.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { useCallback } from "react";
+import { useFonts } from "expo-font";
+
+import Login from "./screens/LoginScreen";
+import Register from "./screens/RegistrationScreen";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
+
+const App = () => {
+  const [fontsLoaded] = useFonts({
+    Roboto: require("./assets/fonts/Roboto/Roboto-Regular.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      {/* <Login /> */}
+      <Register />
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: { flex: 1, fontFamily: "Roboto" },
+});
